067f278b7e12211633515195f5aa7e27870f14ef
Divin Ngenzi <divinngenzi20@gmail.com>
2026-02-02 18:47:16 +0200

    MoFresh System

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import Home from "@/pages/Home"
+import { Route, Routes } from "react-router"
+
+
+function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+
+      
+        
+    </Routes>
+  )
+}
+
+export default AppRoutes
