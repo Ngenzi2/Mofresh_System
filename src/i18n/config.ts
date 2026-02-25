93a7fcee0cb6b13f1fb483127fe68c28793690d6
Ngenzi Divin <divinngenzi20@gmail.com>
2026-02-25 10:32:43 +0200

    Client png

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -137,7 +137,7 @@ const resources = {
       mofreshHub: 'MoFresh Off-Grid Hub',
       yourMessage: 'Your Message',
       fullName: 'Full Name',
-      fullNamePlaceholder: 'John Doe',
+      fullNamePlaceholder: 'MUNEZERO Aline',
       emailAddressLabel: 'Email Address',
 
       // How it Works Steps
